927b797c587d602ce26e030fee05779362ab2359
takumi-nakagawa-1016 <takumi.nakagawa@arsaga.jp>
2023-11-24 15:56:33 +0900

    Initial Commit

---
+++ mile&task/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mile&task
+//
+//  Created by 中川 匠 on 2023/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mile&task/mile_taskApp.swift
@@ -0,0 +1,17 @@
+//
+//  mile_taskApp.swift
+//  mile&task
+//
+//  Created by 中川 匠 on 2023/11/24.
+//
+
+import SwiftUI
+
+@main
+struct mile_taskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
